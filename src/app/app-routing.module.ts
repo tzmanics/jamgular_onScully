2a0b3dae5f938874c614f3bbc7955c820c6b5d0b
Tara Z. Manicsic <tzmanics@gmail.com>
2019-12-13 22:18:50 -0500

    adds routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,35 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-
-const routes: Routes = [{ path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }, { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) }, { path: 'tutorials', loadChildren: () => import('./tutorials/tutorials.module').then(m => m.TutorialsModule) }, { path: 'message-boardd', loadChildren: () => import('./message-board/message-board.module').then(m => m.MessageBoardModule) }, { path: 'user', loadChildren: () => import('./users/users.module').then(m => m.UsersModule) }];
+const routes: Routes = [
+  {
+    path: "home",
+    loadChildren: () => import("./home/home.module").then(m => m.HomeModule)
+  },
+  {
+    path: "about",
+    loadChildren: () => import("./about/about.module").then(m => m.AboutModule)
+  },
+  {
+    path: "tutorials",
+    loadChildren: () =>
+      import("./tutorials/tutorials.module").then(m => m.TutorialsModule)
+  },
+  {
+    path: "message-board",
+    loadChildren: () =>
+      import("./message-board/message-board.module").then(
+        m => m.MessageBoardModule
+      )
+  },
+  {
+    path: "users",
+    loadChildren: () => import("./users/users.module").then(m => m.UsersModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
